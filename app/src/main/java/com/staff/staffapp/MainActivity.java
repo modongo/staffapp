022dbde9784fe3e4e8db350927a1f3e5bf9fce77
George Kenyanga. Mogaka <GMOGAKA@safaricom.co.ke>
2019-06-14 15:03:44 +0300

    Before merge

--- app/src/main/java/com/staff/staffapp/MainActivity.java
+++ app/src/main/java/com/staff/staffapp/MainActivity.java
@@ -1,14 +1,31 @@
 package com.staff.staffapp;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ProductsServices.class);
+                startActivity(intent);
+//                Toast.makeText(MainActivity.this, location, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/staff/staffapp/ProductsServices.java
@@ -0,0 +1,5 @@
+package com.staff.staffapp;
+
+public class ProductsServices {
+
+}
