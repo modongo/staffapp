f3492becbef7508830f34569299ecc7339c26b8c
Michael Odongo <mikodongo@gmail.com>
2019-07-02 17:20:09 +0300

    updated fragments and display layouts  & adapter

--- app/src/main/java/com/staff/staffapp/ui/ChatActivity.java
+++ app/src/main/java/com/staff/staffapp/ui/ChatActivity.java
@@ -2,6 +2,8 @@ package com.staff.staffapp.ui;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.View;
@@ -12,15 +14,22 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.staff.staffapp.R;
+import com.staff.staffapp.adapter.ChatTabsAccessorAdapter;
 
 
 public class ChatActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+    private ChatTabsAccessorAdapter mTabsAccessorAdapter;
 
     // TODO: Add member variables here:
     private String mDisplayName;
@@ -32,13 +41,28 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+
+        // TODO: List page header fragment
+
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Chat");
+
+        myViewPager = findViewById(R.id.main_tabs_pager);
+        mTabsAccessorAdapter = new ChatTabsAccessorAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(mTabsAccessorAdapter);
+
+        myTabLayout = findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
+
         // TODO: Set up the display name and get the Firebase reference
 
 
         // Link the Views in the layout to the Java code
-        mInputText = (EditText) findViewById(R.id.messageInput);
-        mSendButton = (ImageButton) findViewById(R.id.sendButton);
-        mChatListView = (ListView) findViewById(R.id.chat_list_view);
+        mInputText = findViewById(R.id.messageInput);
+        mSendButton = findViewById(R.id.sendButton);
+        mChatListView = findViewById(R.id.chat_list_view);
 
         // TODO: Send the message when the "enter" button is pressed
 
