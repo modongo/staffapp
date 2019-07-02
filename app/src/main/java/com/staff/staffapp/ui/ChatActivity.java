4bd819cb416435096656aee83e38bebd120fabd7
Michael Odongo <mikodongo@gmail.com>
2019-07-02 04:52:28 +0300

    updated chatactivity.java & strings.xml

--- app/src/main/java/com/staff/staffapp/ui/ChatActivity.java
+++ app/src/main/java/com/staff/staffapp/ui/ChatActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,52 +22,49 @@ import com.staff.staffapp.R;
 
 public class ChatActivity extends AppCompatActivity {
 
-    private DatabaseReference myDatabase;
-//    private Button mSendButton;
+    // TODO: Add member variables here:
+    private String mDisplayName;
+    private ListView mChatListView;
+    private EditText mInputText;
+    private ImageButton mSendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-
-        myDatabase = FirebaseDatabase.getInstance().getReference("Message");
-
-        final TextView myText = findViewById(R.id.editText);
-
-        myDatabase.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                myText.setText(dataSnapshot.getValue().toString());
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                myText.setText("CANCELLED");
-
-            }
-        });
+        // TODO: Set up the display name and get the Firebase reference
 
 
+        // Link the Views in the layout to the Java code
+        mInputText = (EditText) findViewById(R.id.messageInput);
+        mSendButton = (ImageButton) findViewById(R.id.sendButton);
+        mChatListView = (ListView) findViewById(R.id.chat_list_view);
 
-//        mSendButton = (Button) findViewById(R.id.sendButton);
-//        mSendButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Toast.makeText(ChatActivity.this, "Message Sent", Toast.LENGTH_LONG).show();
-//
-//            }
-//        });
+        // TODO: Send the message when the "enter" button is pressed
+
+
+        // TODO: Add an OnClickListener to the sendButton to send a message
+
     }
 
-    public void sendButton(View view) {
-        EditText myEditText = findViewById(R.id.editText);
-        myDatabase.push().setValue(myEditText.getText().toString());
-        myEditText.setText("");
+    // TODO: Retrieve the display name from the Shared Preferences
+
+
+    private void sendMessage() {
+
+        // TODO: Grab the text the user typed in and push the message to Firebase
+
     }
 
+    // TODO: Override the onStart() lifecycle method. Setup the adapter here.
 
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // TODO: Remove the Firebase event listener on the adapter.
+
+    }
 
 }
