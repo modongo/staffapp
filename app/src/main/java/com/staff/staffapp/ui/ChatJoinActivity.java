f0ea8aa6d650809b6259d79ac77c6b54c480f1ff
Michael Odongo <mikodongo@gmail.com>
2019-07-02 03:19:28 +0300

    added chat join activity

---
+++ app/src/main/java/com/staff/staffapp/ui/ChatJoinActivity.java
@@ -0,0 +1,16 @@
+package com.staff.staffapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.staff.staffapp.R;
+
+public class ChatJoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_join);
+    }
+}
