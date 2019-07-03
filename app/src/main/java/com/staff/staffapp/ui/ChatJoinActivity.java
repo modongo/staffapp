3c1c1283dfbba1f8be3123fc459a9677e92f31ee
Michael Odongo <mikodongo@gmail.com>
2019-07-03 12:58:27 +0300

    added phonelogin activity,updated chatjoin activity,added drawable assets and updated requisite layouts

--- app/src/main/java/com/staff/staffapp/ui/ChatJoinActivity.java
+++ app/src/main/java/com/staff/staffapp/ui/ChatJoinActivity.java
@@ -1,16 +1,14 @@
 package com.staff.staffapp.ui;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.KeyEvent;
+import android.text.TextUtils;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,96 +17,132 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.staff.staffapp.R;
 
 public class ChatJoinActivity extends AppCompatActivity {
 
     // TODO: Add member variables here:
     private FirebaseAuth mAuth;
-    // UI references.
-    private AutoCompleteTextView mEmailView;
-    private EditText mPasswordView;
+    private ProgressDialog loadingBar;
+
+    private FirebaseUser currentUser;
+
+    private Button LoginButton, PhoneLoginButton;
+    private EditText UserEMail, UserPassword;
+    private TextView NeedNewAccountLink, ForgetPasswordLink;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat_join);
-
-        mEmailView = findViewById(R.id.login_email);
-        mPasswordView = findViewById(R.id.login_password);
-
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == R.integer.login || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
-                    return true;
-                }
-                return false;
-            }
-        });
-
-        // TODO: Grab an instance of FirebaseAuth
+        setContentView(R.layout.activity_login_alt);
+//   Grab an instance of FirebaseAuth
         mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
 
-    }
+        initializeFields();
 
-    // Executed when Sign in button pressed
-    public void signInExistingUser(View v)   {
-        // TODO: Call attemptLogin() here
-        attemptLogin();
-
-    }
-
-    // Executed when Register button pressed
-    public void registerNewUser(View v) {
-        Intent intent = new Intent(this,ChatRegisterActivity.class);
-        finish();
-        startActivity(intent);
-    }
-
-    // TODO: Complete the attemptLogin() method
-    private void attemptLogin() {
-
-        String email = mEmailView.getText().toString();
-        String password = mPasswordView.getText().toString();
-
-        if (email.isEmpty())
-            if (email.equals("") || password.equals("")) return;
-        Toast.makeText(this, "Login in progress...", Toast.LENGTH_SHORT).show();
-
-        // TODO: Use FirebaseAuth to sign in with email & password
-        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-
-                Log.d("staffChat", "signInWithEmail() onComplete: " + task.isSuccessful());
-
-                if (!task.isSuccessful()) {
-                    Log.d("staffChat", "Problem signing in: " + task.getException());
-                    showErrorDialog("There was a problem signing in");
-                } else {
-                    Intent intent = new Intent(ChatJoinActivity.this, ChatActivity.class);
-                    finish();
-                    startActivity(intent);
-                }
-
+            public void onClick(View v) {
+                SendUserToRegisterActivity();
             }
         });
 
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AllowUserToLogin();
+            }
+        });
 
+        PhoneLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent phoneLoginIntent = new Intent(ChatJoinActivity.this, PhoneLoginActivity.class);
+                startActivity(phoneLoginIntent);
+            }
+        });
     }
 
-    // TODO: Show error on screen with an alert dialog
-    private void showErrorDialog(String message) {
+    //check if user exists otherwise send to list activity
 
-        new AlertDialog.Builder(this)
-                .setTitle("Oops")
-                .setMessage(message)
-                .setPositiveButton(android.R.string.ok, null)
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .show();
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (currentUser != null) {
+            SendUserToMainActivity();
+        }
     }
 
+
+    // allow users login
+
+    private void AllowUserToLogin() {
+        String email = UserEMail.getText().toString();
+        String password = UserPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter email...", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter password...", Toast.LENGTH_SHORT).show();
+        } else {
+            loadingBar.setTitle("Sign In");
+            loadingBar.setMessage("Please wait...");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()) {
+                                SendUserToMainActivity();
+                                Toast.makeText(ChatJoinActivity.this, "Logged in Successful...", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            } else {
+                                String message = task.getException().toString();
+                                Toast.makeText(ChatJoinActivity.this, "Error : " + message, Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+
+                        }
+                    });
+        }
+    }
+
+
+    //initialize the view fields accordingly
+
+    private void initializeFields() {
+        LoginButton = findViewById(R.id.login_button);
+        PhoneLoginButton = findViewById(R.id.phone_login_button);
+        UserEMail = findViewById(R.id.login_email);
+        UserPassword = findViewById(R.id.login_password);
+        NeedNewAccountLink = findViewById(R.id.need_new_account_link);
+        ForgetPasswordLink = findViewById(R.id.forget_password_link);
+        loadingBar = new ProgressDialog(this);
+    }
+
+// send user to login main activity (its listActivity  for our case)
+
+    private void SendUserToMainActivity() {
+        Intent mainIntent = new Intent(ChatJoinActivity.this, ListActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
+    }
+
+
+// send user to register method
+
+    private void SendUserToRegisterActivity() {
+        Intent registerIntent = new Intent(ChatJoinActivity.this, ChatRegisterActivity.class);
+        startActivity(registerIntent);
+    }
 
 }
