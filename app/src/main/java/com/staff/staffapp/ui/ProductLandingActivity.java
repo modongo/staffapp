443a7389afed28a7628b77333ea1af8a6f12246b
mopiata <mopiata@safaricomdigitalacademy.co.ke>
2019-07-03 13:56:27 +0300

    created products landing page

--- app/src/main/java/com/staff/staffapp/ui/ProductLandingActivity.java
+++ app/src/main/java/com/staff/staffapp/ui/ProductLandingActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.staff.staffapp.R;
+
 public class ProductLandingActivity extends AppCompatActivity {
 
     @Override
