ac66e4581e306064d24a59184990073cf56e9115
Michael Odongo <mikodongo@gmail.com>
2019-07-03 08:30:30 +0300

    updated custom chat bar & chatactivity, messages

--- app/src/main/java/com/staff/staffapp/ui/ChatActivity.java
+++ app/src/main/java/com/staff/staffapp/ui/ChatActivity.java
@@ -1,14 +1,20 @@
 package com.staff.staffapp.ui;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,140 +23,186 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserInfo;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 import com.staff.staffapp.R;
 import com.staff.staffapp.adapter.ChatListAdapter;
 import com.staff.staffapp.adapter.ChatTabsAccessorAdapter;
+import com.staff.staffapp.adapter.MessageAdapter;
 import com.staff.staffapp.model.InstantMessage;
+import com.staff.staffapp.model.Messages;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class ChatActivity extends AppCompatActivity {
 
-    private Toolbar mToolbar;
-    private ViewPager myViewPager;
-    private TabLayout myTabLayout;
-    private ChatTabsAccessorAdapter mTabsAccessorAdapter;
-
     // Add member variables here:
-    private String mDisplayName;
-    private ListView mChatListView;
-    private EditText mInputText;
-    private ImageButton mSendButton;
-    private DatabaseReference mDatabaseReference;
-    private ChatListAdapter mAdapter;
 
+    private String messageReceiverID, messageReceiverName, MessageReceiverImage, messageSenderID;
+
+    private TextView userName, userLastSeen;
+    private CircleImageView userImage;
+
+    private Toolbar ChatToolBar;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference RootRef;
+
+    private ImageButton SendMessageButton;
+    private EditText MessageInputText;
+
+    private final List<Messages> messagesList = new ArrayList<>();
+    private LinearLayoutManager mLinearLayoutManager;
+    private MessageAdapter mMessageAdapter;
+    private RecyclerView userMessagesList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        mAuth = FirebaseAuth.getInstance();
+        messageSenderID = mAuth.getCurrentUser().getUid();
+        RootRef = FirebaseDatabase.getInstance().getReference();
 
-        // TODO: List page header fragment
+        messageReceiverID = getIntent().getExtras().get("visit_user_id").toString();
+        messageReceiverName = getIntent().getExtras().get("visit_user_name").toString();
+        MessageReceiverImage = getIntent().getExtras().get("visit_image").toString();
 
-        mToolbar = findViewById(R.id.main_page_toolbar);
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setTitle("Chat");
-
-        myViewPager = findViewById(R.id.main_tabs_pager);
-        mTabsAccessorAdapter = new ChatTabsAccessorAdapter(getSupportFragmentManager());
-        myViewPager.setAdapter(mTabsAccessorAdapter);
-
-        myTabLayout = findViewById(R.id.main_tabs);
-        myTabLayout.setupWithViewPager(myViewPager);
-
-        // Set up the display name and get the Firebase reference
-
-        setupDisplayName();
-        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
-
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        UserInfo profile = user.getProviderData().get(0); // getting the first one bc this is a list
-        String uid = profile.getUid();
-        Log.d("StaffChat", "User id is: " + uid);
-
-        // Link the Views in the layout to the Java code
-        mInputText = findViewById(R.id.messageInput);
-        mSendButton = findViewById(R.id.sendButton);
-        mChatListView = findViewById(R.id.chat_list_view);
-
-        //  Send the message when the "enter" button is pressed
-
-        mInputText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-
-                sendMessage();
-                return true;
-            }
-        });
+        intializeControllers();
 
 
-        // Add an OnClickListener to the sendButton to send a message
-
-        mSendButton.setOnClickListener(new View.OnClickListener() {
+        SendMessageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendMessage();
+                SendMessage();
+
             }
         });
-
-    }
-
-    // Retrieve the display name from the Shared Preferences
-
-    private void setupDisplayName() {
-
-        SharedPreferences prefs = getSharedPreferences(ChatRegisterActivity.CHAT_PREFS, MODE_PRIVATE);
-
-        mDisplayName = prefs.getString(ChatRegisterActivity.DISPLAY_NAME_KEY, null);
-
-        if (mDisplayName == null) mDisplayName = "Anonymous";
     }
 
 
-    private void sendMessage() {
+    // Initialize controller for toolbar etc
 
-        Log.d("StaffChat", "I sent something");
+    private void intializeControllers() {
+        ChatToolBar = findViewById(R.id.chat_toolbar);
+        setSupportActionBar(ChatToolBar);
 
-        //  Grab the text the user typed in and push the message to Firebase
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
 
-        String input = mInputText.getText().toString();
-        if (!input.equals("")) {
-            InstantMessage chat = new InstantMessage(input, mDisplayName);
-            mDatabaseReference.child("messages").push().setValue(chat);
-            mInputText.setText("");
+        LayoutInflater layoutInflater =  (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View actionBarView = layoutInflater.inflate(R.layout.custom_chat_bar, null);
+        actionBar.setCustomView(actionBarView);
+
+        userImage = findViewById(R.id.custom_profile_image);
+        userName = findViewById(R.id.custom_profile_name);
+        userLastSeen = findViewById(R.id.custom_user_last_seen);
+
+        SendMessageButton = findViewById(R.id.send_message_btn);
+        MessageInputText = findViewById(R.id.input_message);
+
+        mMessageAdapter = new MessageAdapter(messagesList);
+        userMessagesList = findViewById(R.id.private_messages_list_of_users);
+
+        mLinearLayoutManager = new LinearLayoutManager(this);
+        userMessagesList.setLayoutManager(mLinearLayoutManager);
+        userMessagesList.setAdapter(mMessageAdapter);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        RootRef.child("Messages").child(messageSenderID).child(messageReceiverID)
+                .addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        Messages messages = dataSnapshot.getValue(Messages.class);
+
+                        messagesList.add(messages);
+
+                        mMessageAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
+    // Send message logic
+    private void SendMessage() {
+        String messageText = MessageInputText.getText().toString();
+
+        if (TextUtils.isEmpty(messageText)) {
+            Toast.makeText(this, "Please type a message.", Toast.LENGTH_SHORT).show();
+        } else {
+            String messageSenderRef = "Messages/" + messageSenderID + "/" + messageReceiverID;
+            String messageReceiverRef = "Messages/" + messageReceiverID + "/" + messageSenderID;
+
+            DatabaseReference userMessageKeyRef = RootRef.child("Messages")
+                    .child(messageSenderID).child(messageReceiverID).push();
+
+            String messagePushID = userMessageKeyRef.getKey();
+
+            Map messageTextBody = new HashMap();
+            messageTextBody.put("message", messageText);
+            messageTextBody.put("type", "text");
+            messageTextBody.put("from", messageSenderID);
+
+            Map messageBodyDetails = new HashMap();
+            messageBodyDetails.put(messageSenderRef + "/" + messagePushID, messageTextBody);
+            messageBodyDetails.put(messageReceiverRef + "/" + messagePushID, messageTextBody);
+
+            RootRef.updateChildren(messageBodyDetails).addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(ChatActivity.this, "Message Sent.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        String message = task.getException().getMessage();
+                        Toast.makeText(ChatActivity.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+                    }
+                    MessageInputText.setText("");
+
+                }
+            });
         }
 
-
     }
-
-    //  Override the onStart() lifecycle method. Setup the adapter here.
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        mAdapter = new ChatListAdapter(this, mDatabaseReference, mDisplayName);
-        mChatListView.setAdapter(mAdapter);
-    }
-
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // Remove the Firebase event listener on the adapter.
-
-        mAdapter.cleanup();
-
-
-    }
-
 }
+
