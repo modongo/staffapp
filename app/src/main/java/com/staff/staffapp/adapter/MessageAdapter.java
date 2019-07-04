6a1fec57f18adde215b095db2e9e07c4c4b36c0b
Michael Odongo <mikodongo@gmail.com>
2019-07-04 09:45:51 +0300

    updated messages logic

--- app/src/main/java/com/staff/staffapp/adapter/MessageAdapter.java
+++ app/src/main/java/com/staff/staffapp/adapter/MessageAdapter.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,43 +23,60 @@ import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder> {
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder>
+{
     private List<Messages> userMessagesList;
     private FirebaseAuth mAuth;
     private DatabaseReference usersRef;
 
-    public MessageAdapter (List<Messages> userMessagesList) {
+
+    public MessageAdapter (List<Messages> userMessagesList)
+    {
         this.userMessagesList = userMessagesList;
     }
 
-    public class MessageViewHolder extends RecyclerView.ViewHolder {
 
+
+    public class MessageViewHolder extends RecyclerView.ViewHolder
+    {
         public TextView senderMessageText, receiverMessageText;
         public CircleImageView receiverProfileImage;
+        public ImageView messageSenderPicture, messageReceiverPicture;
 
-        public MessageViewHolder(@NonNull View itemView) {
+
+        public MessageViewHolder(@NonNull View itemView)
+        {
             super(itemView);
 
-            senderMessageText = itemView.findViewById(R.id.sender_message_text);
-            receiverMessageText = itemView.findViewById(R.id.receiver_message_text);
-            receiverProfileImage = itemView.findViewById(R.id.message_profile_image);
+            senderMessageText = (TextView) itemView.findViewById(R.id.sender_messsage_text);
+            receiverMessageText = (TextView) itemView.findViewById(R.id.receiver_message_text);
+            receiverProfileImage = (CircleImageView) itemView.findViewById(R.id.message_profile_image);
+            messageReceiverPicture = itemView.findViewById(R.id.message_receiver_image_view);
+            messageSenderPicture = itemView.findViewById(R.id.message_sender_image_view);
         }
     }
 
+
+
+
     @NonNull
     @Override
-    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i)
+    {
         View view = LayoutInflater.from(viewGroup.getContext())
                 .inflate(R.layout.custom_messages_layout, viewGroup, false);
 
         mAuth = FirebaseAuth.getInstance();
+
         return new MessageViewHolder(view);
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull final MessageViewHolder messageViewHolder, int i) {
 
-        String messageSenderID = mAuth.getCurrentUser().getUid();
+
+    @Override
+    public void onBindViewHolder(@NonNull final MessageViewHolder messageViewHolder, int i)
+    {
+        String messageSenderId = mAuth.getCurrentUser().getUid();
         Messages messages = userMessagesList.get(i);
 
         String fromUserID = messages.getFrom();
@@ -68,12 +86,14 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
 
         usersRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                if (dataSnapshot.hasChild("image")) {
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                if (dataSnapshot.hasChild("image"))
+                {
                     String receiverImage = dataSnapshot.child("image").getValue().toString();
+
                     Picasso.get().load(receiverImage).placeholder(R.drawable.profile_image).into(messageViewHolder.receiverProfileImage);
                 }
-
             }
 
             @Override
@@ -82,31 +102,46 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
             }
         });
 
-        if (fromMessageType.equals("text")) {
-            messageViewHolder.receiverMessageText.setVisibility(View.INVISIBLE);
-            messageViewHolder.receiverProfileImage.setVisibility(View.INVISIBLE);
 
-            if (fromUserID.equals(messageSenderID)) {
+
+
+
+        messageViewHolder.receiverMessageText.setVisibility(View.GONE);
+        messageViewHolder.receiverProfileImage.setVisibility(View.GONE);
+        messageViewHolder.senderMessageText.setVisibility(View.GONE);
+        messageViewHolder.messageSenderPicture.setVisibility(View.GONE);
+        messageViewHolder.messageReceiverPicture.setVisibility(View.GONE);
+
+
+        if (fromMessageType.equals("text"))
+        {
+            if (fromUserID.equals(messageSenderId))
+            {
+                messageViewHolder.senderMessageText.setVisibility(View.VISIBLE);
+
                 messageViewHolder.senderMessageText.setBackgroundResource(R.drawable.sender_messages_layout);
                 messageViewHolder.senderMessageText.setTextColor(Color.BLACK);
-                messageViewHolder.senderMessageText.setText(messages.getMessage());
-            } else {
-                messageViewHolder.senderMessageText.setVisibility(View.INVISIBLE);
-
-                messageViewHolder.receiverMessageText.setVisibility(View.VISIBLE);
+                messageViewHolder.senderMessageText.setText(messages.getMessage() + "\n \n" + messages.getTime() + " - " + messages.getDate());
+            }
+            else
+            {
                 messageViewHolder.receiverProfileImage.setVisibility(View.VISIBLE);
+                messageViewHolder.receiverMessageText.setVisibility(View.VISIBLE);
 
                 messageViewHolder.receiverMessageText.setBackgroundResource(R.drawable.receiver_messages_layout);
                 messageViewHolder.receiverMessageText.setTextColor(Color.BLACK);
-                messageViewHolder.receiverMessageText.setText(messages.getMessage());
+                messageViewHolder.receiverMessageText.setText(messages.getMessage() + "\n \n" + messages.getTime() + " - " + messages.getDate());
             }
         }
     }
 
+
+
+
     @Override
-    public int getItemCount() {
+    public int getItemCount()
+    {
         return userMessagesList.size();
     }
-
 
 }
