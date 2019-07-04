3a53d4d90b2a0bcb954c0f529e3444ffe94fc9eb
Michael Odongo <mikodongo@gmail.com>
2019-07-04 09:20:43 +0300

    updated ChatJoin activity

--- app/src/main/java/com/staff/staffapp/ui/ChatJoinActivity.java
+++ app/src/main/java/com/staff/staffapp/ui/ChatJoinActivity.java
@@ -19,138 +19,162 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.staff.staffapp.R;
 
 public class ChatJoinActivity extends AppCompatActivity {
 
-    //  Add member variables here:
-    private FirebaseAuth mAuth;
-    private ProgressDialog loadingBar;
+        private FirebaseAuth mAuth;
+        private ProgressDialog loadingBar;
 
-    private FirebaseUser currentUser;
+        private Button LoginButton, PhoneLoginButton;
+        private EditText UserEmail, UserPassword;
+        private TextView NeedNewAccountLink, ForgetPasswordLink;
 
-    private Button LoginButton, PhoneLoginButton;
-    private EditText UserEMail, UserPassword;
-    private TextView NeedNewAccountLink, ForgetPasswordLink;
+        private DatabaseReference UsersRef;
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_alt);
-//   Grab an instance of FirebaseAuth
-        mAuth = FirebaseAuth.getInstance();
-        currentUser = mAuth.getCurrentUser();
+        @Override
+        protected void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_chat_join);
 
-        initializeFields();
-        Log.d("SfcChat", "initializeFields");
+            mAuth = FirebaseAuth.getInstance();
+            UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
 
-        NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SendUserToRegisterActivity();
-            }
-        });
 
-        LoginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AllowUserToLogin();
-            }
-        });
+            InitializeFields();
 
-        PhoneLoginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent phoneLoginIntent = new Intent(ChatJoinActivity.this, PhoneLoginActivity.class);
-                startActivity(phoneLoginIntent);
-            }
-        });
-    }
 
-    //check if user exists otherwise send to list activity
+            NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view)
+                {
+                    SendUserToRegisterActivity();
+                }
+            });
 
-    @Override
-    protected void onStart() {
-        super.onStart();
 
-        if (currentUser != null) {
-            SendUserToMainActivity();
+            LoginButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view)
+                {
+                    AllowUserToLogin();
+                }
+            });
 
-            Log.d("SfcChat", "****SendUserToMainActivity****");
-
+            PhoneLoginButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view)
+                {
+                    Intent phoneLoginIntent = new Intent(ChatJoinActivity.this, PhoneLoginActivity.class);
+                    startActivity(phoneLoginIntent);
+                }
+            });
         }
-    }
 
 
-    // allow users login
 
-    private void AllowUserToLogin() {
-        String email = UserEMail.getText().toString();
-        String password = UserPassword.getText().toString();
 
-        if (TextUtils.isEmpty(email)) {
-            Toast.makeText(this, "Please enter email...", Toast.LENGTH_SHORT).show();
+        @Override
+        protected void onStart()
+        {
+            super.onStart();
+
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+            if (currentUser != null)
+            {
+                SendUserToMainActivity();
+            }
         }
-        if (TextUtils.isEmpty(password)) {
-            Toast.makeText(this, "Please enter password...", Toast.LENGTH_SHORT).show();
-        } else {
-            loadingBar.setTitle("Sign In");
-            loadingBar.setMessage("Please wait...");
-            loadingBar.setCanceledOnTouchOutside(true);
-            loadingBar.show();
 
-            mAuth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
 
-                            if (task.isSuccessful()) {
-                                SendUserToMainActivity();
-                                Toast.makeText(ChatJoinActivity.this, "Logged in Successful...", Toast.LENGTH_SHORT).show();
-                                loadingBar.dismiss();
-                            } else {
-                                String message = task.getException().toString();
-                                Toast.makeText(ChatJoinActivity.this, "Error : " + message, Toast.LENGTH_SHORT).show();
-                                loadingBar.dismiss();
+
+        private void AllowUserToLogin()
+        {
+            String email = UserEmail.getText().toString();
+            String password = UserPassword.getText().toString();
+
+            if (TextUtils.isEmpty(email))
+            {
+                Toast.makeText(this, "Please enter email...", Toast.LENGTH_SHORT).show();
+            }
+            if (TextUtils.isEmpty(password))
+            {
+                Toast.makeText(this, "Please enter password...", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                loadingBar.setTitle("Sign In");
+                loadingBar.setMessage("Please wait....");
+                loadingBar.setCanceledOnTouchOutside(true);
+                loadingBar.show();
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task)
+                            {
+                                if (task.isSuccessful())
+                                {
+                                    String currentUserId = mAuth.getCurrentUser().getUid();
+                                    String deviceToken = FirebaseInstanceId.getInstance().getToken();
+
+                                    UsersRef.child(currentUserId).child("device_token")
+                                            .setValue(deviceToken)
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task)
+                                                {
+                                                    if (task.isSuccessful())
+                                                    {
+                                                        SendUserToMainActivity();
+                                                        Toast.makeText(ChatJoinActivity.this, "Logged in Successful...", Toast.LENGTH_SHORT).show();
+                                                        loadingBar.dismiss();
+                                                    }
+                                                }
+                                            });
+                                }
+                                else
+                                {
+                                    String message = task.getException().toString();
+                                    Toast.makeText(ChatJoinActivity.this, "Error : " + message, Toast.LENGTH_SHORT).show();
+                                    loadingBar.dismiss();
+                                }
                             }
+                        });
+            }
+        }
 
-                        }
-                    });
+
+
+        private void InitializeFields()
+        {
+            LoginButton = (Button) findViewById(R.id.login_button);
+            PhoneLoginButton = (Button) findViewById(R.id.phone_login_button);
+            UserEmail = (EditText) findViewById(R.id.login_email);
+            UserPassword = (EditText) findViewById(R.id.login_password);
+            NeedNewAccountLink = (TextView) findViewById(R.id.need_new_account_link);
+            ForgetPasswordLink = (TextView) findViewById(R.id.forget_password_link);
+            loadingBar = new ProgressDialog(this);
+        }
+
+
+
+        private void SendUserToMainActivity()
+        {
+            Intent mainIntent = new Intent(ChatJoinActivity.this, ChatLoginActivity.class);
+            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(mainIntent);
+            finish();
+        }
+
+        private void SendUserToRegisterActivity()
+        {
+            Intent registerIntent = new Intent(ChatJoinActivity.this, ChatRegisterActivity.class);
+            startActivity(registerIntent);
         }
     }
-
-
-    //initialize the view fields accordingly
-
-    private void initializeFields() {
-        LoginButton = findViewById(R.id.login_button);
-        PhoneLoginButton = findViewById(R.id.phone_login_button);
-        UserEMail = findViewById(R.id.login_email);
-        UserPassword = findViewById(R.id.login_password);
-        NeedNewAccountLink = findViewById(R.id.need_new_account_link);
-        ForgetPasswordLink = findViewById(R.id.forget_password_link);
-        loadingBar = new ProgressDialog(this);
-
-        Log.d("SfcChat", "initializeFields Yeah");
-
-    }
-
-// send user to login main activity (its listActivity  for our case)
-
-    private void SendUserToMainActivity() {
-        Intent mainIntent = new Intent(ChatJoinActivity.this, ChatLoginActivity.class);
-        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(mainIntent);
-        finish();
-    }
-
-
-// send user to register method
-
-    private void SendUserToRegisterActivity() {
-        Intent registerIntent = new Intent(ChatJoinActivity.this, ChatRegisterActivity.class);
-        startActivity(registerIntent);
-    }
-
-}
