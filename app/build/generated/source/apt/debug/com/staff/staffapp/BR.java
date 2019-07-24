c5f588f46d7aae561daff9a3a436716780ade755
SamNgigi <sam.t.ngigi@gmail.com>
2019-07-24 09:05:05 +0300

    [git-fix]

--- app/build/generated/source/apt/debug/com/staff/staffapp/BR.java
+++
@@ -1,9 +0,0 @@
-package com.staff.staffapp;
-
-public class BR {
-  public static final int _all = 0;
-
-  public static final int bgColor = 1;
-
-  public static final int myName = 2;
-}
--- app/build/generated/source/dataBinding/trigger/debug/com/staff/staffapp/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package com.staff.staffapp;
-
-import androidx.databinding.BindingBuildInfo;
-
-@BindingBuildInfo
-public class DataBindingInfo {}
